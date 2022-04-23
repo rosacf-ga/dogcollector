962a158ea3c964b40e872b60643052c212c766d9
rosacf-ga <rc.floress@yahoo.com>
2022-04-22 19:01:45 -0700

    Create paths and functions to render html templates

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+class Dog:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+dogs = [
+  Dog('Princess', 'chihuahua', '12 lb devil', 15),
+  Dog('Captain', 'terrier mix', 'kind and chunky white terrier', 10),
+  Dog('Keanu', 'cardigan corgi', 'energetic black puppy', 0),
+]
 
 # Create your views here.
+def home(req):
+  return HttpResponse('Home page!')
+
+def about(req):
+  return render(req, 'about.html')
+
+def dogs_index(req):
+  return render(req, 'dogs/index.html', {'dogs': dogs})
