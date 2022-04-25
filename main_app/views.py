08ae10a9c82fb982217704851e1f0ed6831f05bb
rosacf-ga <rc.floress@yahoo.com>
2022-04-25 13:41:34 -0700

    Create path and function for details page

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-class Dog:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-dogs = [
-  Dog('Princess', 'chihuahua', '12 lb devil', 15),
-  Dog('Captain', 'terrier mix', 'kind and chunky white terrier', 10),
-  Dog('Keanu', 'cardigan corgi', 'energetic black puppy', 0),
-]
+from .models import Dog
 
 # Create your views here.
 def home(req):
@@ -22,4 +10,9 @@ def about(req):
   return render(req, 'about.html')
 
 def dogs_index(req):
+  dogs = Dog.objects.all()
   return render(req, 'dogs/index.html', {'dogs': dogs})
+
+def dogs_detail(req, dog_id):
+  dog = Dog.objects.get(id=dog_id)
+  return render(req, 'dogs/detail.html', {'dog': dog})
