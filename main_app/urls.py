9ce27adc54b2c7d477331aa6df2dff4bad3f5ce7
rosacf-ga <rc.floress@yahoo.com>
2022-04-27 15:30:20 -0700

    Add paths to toy pages

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
 from . import views 
 
+#use pk instead of dog_id/toy_id when using cbvs
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
@@ -9,5 +10,11 @@ urlpatterns = [
   path('dogs/create', views.DogCreate.as_view(), name='dogs_create'),
   path('dogs/<int:pk>/update/', views.DogUpdate.as_view(), name='dogs_update'),
   path('dogs/<int:pk>/delete/', views.DogDelete.as_view(), name='dogs_delete'), 
-  path('dogs/<int:dog_id>/add_feeding/', views.add_feeding, name='add_feeding')
+  path('dogs/<int:dog_id>/add_feeding/', views.add_feeding, name='add_feeding'),
+  path('dogs/<int:dog_id>/assoc_toy/<int:toy_id>', views.assoc_toy, name='assoc_toy'),
+  path('toys/', views.ToyList.as_view(), name='toys_index'),
+  path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
+  path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
+  path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
+  path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Dog
+from .models import Dog, Toy
 from .forms import FeedingForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 
 class DogCreate(CreateView):
   model = Dog 
@@ -15,6 +16,24 @@ class DogUpdate(UpdateView):
 class DogDelete(DeleteView):
   model = Dog 
   success_url = '/dogs/'
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
 
 # Create your views here.
 def home(req):
@@ -29,8 +48,9 @@ def dogs_index(req):
 
 def dogs_detail(req, dog_id):
   dog = Dog.objects.get(id=dog_id)
+  toys_dog_doesnt_have = Toy.objects.exclude(id__in = dog.toys.all().values_list('id'))
   feeding_form = FeedingForm()
-  return render(req, 'dogs/detail.html', {'dog': dog, 'feeding_form': feeding_form})
+  return render(req, 'dogs/detail.html', {'dog': dog, 'feeding_form': feeding_form, 'toys': toys_dog_doesnt_have})
 
 def add_feeding(request, dog_id):
   form = FeedingForm(request.POST)
@@ -38,4 +58,8 @@ def add_feeding(request, dog_id):
     new_feeding = form.save(commit=False)
     new_feeding.dog_id = dog_id 
     new_feeding.save()
+  return redirect('detail', dog_id=dog_id)
+  
+def assoc_toy(request, dog_id, toy_id):
+  Dog.objects.get(id=dog_id).toys.add(toy_id)
   return redirect('detail', dog_id=dog_id)
